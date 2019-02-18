01651b688963b2cc9ac1f28ea32f3a148d77ac64
zohaib10 <zohaib.m.ahmad@gmail.com>
2019-02-17 16:51:47 -0800

    adding stuff

---
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          LeadManager
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Header;
